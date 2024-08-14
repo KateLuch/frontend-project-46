abb23b00a5ef5e15a1b67afa8d15b4816f29f8a1
Kate Luchanova <ch.katyuha@gmail.com>
2024-08-14 15:54:34 -0400

    step 2, add args and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .argument('<filepath1>', 'file 1')
-  .argument('<filepath2>', 'file 2')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-f, --format [type]', 'output format');
     
 program.parse(process.argv);
